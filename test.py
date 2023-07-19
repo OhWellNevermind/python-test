866905e67dd5f3e60fc6bd3b674865c592cd71f0
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-07-19 17:09:16 +0300

    directory creation removed

--- test.py
+++ test.py
@@ -61,17 +61,21 @@ voices = {
     'Guy': 'en-US-GuyNeural',
 }
 
-label = customtkinter.CTkLabel(master=generate_voice, text="Generate Voice", font=("Roboto", 24, 'bold'))
+label = customtkinter.CTkLabel(
+    master=generate_voice, text="Generate Voice", font=("Roboto", 24, 'bold'))
 label.pack(pady=15, padx=10)
+
 
 def script_upload():
     global script_location
-    script_location = filedialog.askopenfilename(title='Upload Script', filetypes=(('Text Files', '*.txt'),), initialdir='./')
+    script_location = filedialog.askopenfilename(
+        title='Upload Script', filetypes=(('Text Files', '*.txt'),), initialdir='./')
 
     file_entry.configure(state=NORMAL)
     file_entry.delete(0, END)
     file_entry.insert(0, script_location)
     file_entry.configure(state=DISABLED)
+
 
 def set_location():
     global audios_location
@@ -81,6 +85,7 @@ def set_location():
     audios_entry.delete(0, END)
     audios_entry.insert(0, audios_location)
     audios_entry.configure(state=DISABLED)
+
 
 def voice_generation():
     script = open(script_location, 'r', encoding='utf-8')
@@ -124,8 +129,9 @@ def voice_generation():
         progress.configure(text=f'{progressValue}%')
 
         if progressValue == 100:
-            tkinter.messagebox.showinfo(title='Done', message='Voice was generated')
-    
+            tkinter.messagebox.showinfo(
+                title='Done', message='Voice was generated')
+
     for fname in os.listdir('./audio_files_raw'):
         if '_processed' in fname:
             continue
@@ -133,29 +139,37 @@ def voice_generation():
             os.remove(os.path.join('./audio_files_raw', fname))
 
 
-file_entry = customtkinter.CTkEntry(master=generate_voice, width=270, font=("Roboto", 15))
+file_entry = customtkinter.CTkEntry(
+    master=generate_voice, width=270, font=("Roboto", 15))
 file_entry.configure(state=DISABLED)
 file_entry.pack(pady=10)
 
-upload_script = customtkinter.CTkButton(master=generate_voice, text='Upload Script', font=("Roboto", 15), command=script_upload)
+upload_script = customtkinter.CTkButton(
+    master=generate_voice, text='Upload Script', font=("Roboto", 15), command=script_upload)
 upload_script.pack(pady=10)
 
-audios_entry = customtkinter.CTkEntry(master=generate_voice, width=270, font=("Roboto", 15))
+audios_entry = customtkinter.CTkEntry(
+    master=generate_voice, width=270, font=("Roboto", 15))
 audios_entry.configure(state=DISABLED)
 audios_entry.pack(pady=10)
 
-audio_location = customtkinter.CTkButton(master=generate_voice, text='Set Location', font=("Roboto", 15), command=set_location)
+audio_location = customtkinter.CTkButton(
+    master=generate_voice, text='Set Location', font=("Roboto", 15), command=set_location)
 audio_location.pack()
 
-voice_label = customtkinter.CTkLabel(master=generate_voice, text="Select Voice", font=("Roboto", 15, 'bold'))
+voice_label = customtkinter.CTkLabel(
+    master=generate_voice, text="Select Voice", font=("Roboto", 15, 'bold'))
 voice_label.pack(pady=10)
 
-voice_menu = customtkinter.CTkComboBox(master=generate_voice, width=270, values=list(voices.keys()))
+voice_menu = customtkinter.CTkComboBox(
+    master=generate_voice, width=270, values=list(voices.keys()))
 voice_menu.pack(pady=10)
+
 
 def start():
     if not script_location or not audios_location:
-        tkinter.messagebox.showerror(title='Error', message='Please set script location and save directory')
+        tkinter.messagebox.showerror(
+            title='Error', message='Please set script location and save directory')
         return
 
     thread = threading.Thread(target=voice_generation)
@@ -163,19 +177,22 @@ def start():
     # start_btn.configure(state=DISABLED)
 
 
-start_btn = customtkinter.CTkButton(master=generate_voice, text='Start', font=("Roboto", 20, 'bold'), width=220, height=40, command=start)
+start_btn = customtkinter.CTkButton(master=generate_voice, text='Start', font=(
+    "Roboto", 20, 'bold'), width=220, height=40, command=start)
 start_btn.pack(pady=25)
 
 progressbar = Progressbar(master=generate_voice, length=300, value=0)
 progressbar.pack(pady=8, padx=8)
 
-progress = customtkinter.CTkLabel(master=generate_voice, text='0%', font=("Roboto", 16, 'bold'))
+progress = customtkinter.CTkLabel(
+    master=generate_voice, text='0%', font=("Roboto", 16, 'bold'))
 progress.pack(pady=5)
 
 # Increase Image Size
 
 images_location = None
 image_size = 1.25
+
 
 def get_images_location():
     global images_location
@@ -193,33 +210,41 @@ def increase_size():
         img = Image.open(img_path)
 
         width, height = img.size
-        new_width, new_height = int(width * image_size), int(height * image_size)
+        new_width, new_height = int(
+            width * image_size), int(height * image_size)
 
         resized_img = img.resize((new_width, new_height))
         resized_img.save(img_path)
 
-        progressValue = round(((i + 1) / len(os.listdir(images_location))) * 100, 1)
+        progressValue = round(
+            ((i + 1) / len(os.listdir(images_location))) * 100, 1)
 
         images_progressbar.configure(value=progressValue)
         images_progress.configure(text=f'{progressValue}%')
 
         if progressValue == 100:
-            tkinter.messagebox.showinfo(title='Done', message='Images size was increased')
+            tkinter.messagebox.showinfo(
+                title='Done', message='Images size was increased')
 
 
-size_label = customtkinter.CTkLabel(master=increase_img_size, text="Increase Image Size", font=("Roboto", 24, 'bold'))
+size_label = customtkinter.CTkLabel(
+    master=increase_img_size, text="Increase Image Size", font=("Roboto", 24, 'bold'))
 size_label.pack(pady=15, padx=10)
 
-image_entry = customtkinter.CTkEntry(master=increase_img_size, width=270, font=("Roboto", 15))
+image_entry = customtkinter.CTkEntry(
+    master=increase_img_size, width=270, font=("Roboto", 15))
 image_entry.configure(state=DISABLED)
 image_entry.pack(pady=10)
 
-image_location = customtkinter.CTkButton(master=increase_img_size, text='Set Location', font=("Roboto", 15), command=get_images_location)
+image_location = customtkinter.CTkButton(
+    master=increase_img_size, text='Set Location', font=("Roboto", 15), command=get_images_location)
 image_location.pack(pady=10)
+
 
 def images_start():
     if not images_location:
-        tkinter.messagebox.showerror(title='Error', message='Please set images location')
+        tkinter.messagebox.showerror(
+            title='Error', message='Please set images location')
         return
 
     thread = threading.Thread(target=increase_size)
@@ -233,26 +258,31 @@ def set_size(value):
     size_slider_label.configure(text=f'Size: {image_size}x')
 
 
-size_slider_label = customtkinter.CTkLabel(master=increase_img_size, text="Size: 1.25x", font=("Roboto", 15, 'bold'))
+size_slider_label = customtkinter.CTkLabel(
+    master=increase_img_size, text="Size: 1.25x", font=("Roboto", 15, 'bold'))
 size_slider_label.pack(pady=6)
 
-size_slider = customtkinter.CTkSlider(master=increase_img_size, from_=1, to=1.5, number_of_steps=10, command=set_size)
+size_slider = customtkinter.CTkSlider(
+    master=increase_img_size, from_=1, to=1.5, number_of_steps=10, command=set_size)
 size_slider.pack()
 size_slider.set(1.25)
 
-images_start_btn = customtkinter.CTkButton(master=increase_img_size, text='Start', font=("Roboto", 20, 'bold'), width=220, height=40, command=images_start)
+images_start_btn = customtkinter.CTkButton(master=increase_img_size, text='Start', font=(
+    "Roboto", 20, 'bold'), width=220, height=40, command=images_start)
 images_start_btn.pack(pady=25)
 
 images_progressbar = Progressbar(master=increase_img_size, length=300, value=0)
 images_progressbar.pack(pady=8, padx=8)
 
-images_progress = customtkinter.CTkLabel(master=increase_img_size, text='0%', font=("Roboto", 16, 'bold'))
+images_progress = customtkinter.CTkLabel(
+    master=increase_img_size, text='0%', font=("Roboto", 16, 'bold'))
 images_progress.pack(pady=5)
 
 # Clear Text
 
 confidence = 0.15
 clear_images_location = None
+
 
 def import_images():
     global clear_images_location
@@ -275,7 +305,8 @@ def get_images_path(image):
 
 
 def delete_text():
-    images = natsorted(list(map(get_images_path, os.listdir(clear_images_location))))
+    images = natsorted(
+        list(map(get_images_path, os.listdir(clear_images_location))))
 
     language = language_menu.get()
 
@@ -300,7 +331,8 @@ def delete_text():
                 basename = os.path.splitext(os.path.basename(name))[0]
                 ext = os.path.splitext(os.path.basename(name))[1]
 
-            name = os.path.join(os.path.dirname(image), basename + str(index) + ext)
+            name = os.path.join(os.path.dirname(
+                image), basename + str(index) + ext)
             shutil.copy(image, name)
             image = name
 
@@ -340,19 +372,23 @@ def delete_text():
                 rects.append((top_right, bottom_left))
 
                 # Draw a rectangle around the text
-                img_rect = cv2.rectangle(img_rect, bottom_left, top_right, (0, 255, 0), 3)
+                img_rect = cv2.rectangle(
+                    img_rect, bottom_left, top_right, (0, 255, 0), 3)
 
                 # Fill text with white rectangle
-                img_temp = cv2.rectangle(img_temp, bottom_left, top_right, (255, 255, 255), -1)
+                img_temp = cv2.rectangle(
+                    img_temp, bottom_left, top_right, (255, 255, 255), -1)
 
                 # Convert temp image to black and white for mask
                 mask = cv2.cvtColor(img_temp, cv2.COLOR_BGR2GRAY)
 
                 # "Content-Fill" using mask (INPAINT_NS vs INPAINT_TELEA)
-                img_inpaint = cv2.inpaint(img_inpaint, mask, 3, cv2.INPAINT_TELEA)
+                img_inpaint = cv2.inpaint(
+                    img_inpaint, mask, 3, cv2.INPAINT_TELEA)
 
                 # Draw a rectangle around the text
-                preview_rect = cv2.rectangle(img_rect, bottom_left, top_right, (0, 255, 0), 3)
+                preview_rect = cv2.rectangle(
+                    img_rect, bottom_left, top_right, (0, 255, 0), 3)
 
                 # Draw confidence level on detected text
                 cv2.putText(preview_rect, str(round(r[2], 2)), bottom_left, cv2.FONT_HERSHEY_SIMPLEX, 0.5,
@@ -365,18 +401,21 @@ def delete_text():
 
         cv2.imwrite(export_path + image.split('/')[-1], img_inpaint)
 
-        progressValue = round(((index + 1) / len(os.listdir(clear_images_location))) * 100, 1)
+        progressValue = round(
+            ((index + 1) / len(os.listdir(clear_images_location))) * 100, 1)
 
         clear_progressbar.configure(value=progressValue)
         clear_progress.configure(text=f'{progressValue}%')
 
         if progressValue == 100:
-            tkinter.messagebox.showinfo(title='Done', message='Text was deleted')
+            tkinter.messagebox.showinfo(
+                title='Done', message='Text was deleted')
 
 
 def clear_start():
     if not clear_images_location:
-        tkinter.messagebox.showerror(title='Error', message='Please set images location')
+        tkinter.messagebox.showerror(
+            title='Error', message='Please set images location')
         return
 
     thread = threading.Thread(target=delete_text)
@@ -385,8 +424,10 @@ def clear_start():
 
 # Make video
 
+
 voices_location = None
 video_images_location = None
+
 
 def get_voices_location():
     global voices_location
@@ -397,6 +438,7 @@ def get_voices_location():
     video_audios_entry.insert(0, voices_location)
     video_audios_entry.configure(state=DISABLED)
 
+
 def get_video_images_location():
     global video_images_location
     video_images_location = filedialog.askdirectory(initialdir='./')
@@ -406,16 +448,15 @@ def get_video_images_location():
     video_image_entry.insert(0, video_images_location)
     video_image_entry.configure(state=DISABLED)
 
+
 def create_video():
     clips = []
-    video_files = 'video_file'
-    
-    if not os.path.exists(video_files):
-        os.makedirs(video_files)
-    
-    voices = ['./audio_files_raw/{}'.format(f) for f in listdir('./audio_files_raw') if isfile(join('./audio_files_raw', f))]
-    images = natsorted(list(['./images/{}'.format(f) for f in listdir('./images') if isfile(join('./images', f))]))
-    
+
+    voices = ['./audio_files_raw/{}'.format(f) for f in listdir(
+        './audio_files_raw') if isfile(join('./audio_files_raw', f))]
+    images = natsorted(list(['./images/{}'.format(f)
+                       for f in listdir('./images') if isfile(join('./images', f))]))
+
     for img, voice in zip(images, voices):
         audio = MP3(voice)
         audio_length = audio.info.length - 0.5
@@ -425,90 +466,110 @@ def create_video():
         audio = AudioFileClip(voice)
         clip = clip.set_audio(audio)
         clips.append(clip)
-    
+
     print(clips)
-    
+
     # clips_fx = [clips[0]]
     # idx = clips[0].duration - 0.5
     # for clip in clips[1:]:
     #     clips_fx.append(slide_in(clip.set_start(idx), clip.duration - 0.5, 'left'))
     #     idx += clip.duration - 0.5
-        
+
     final_clip = concatenate_videoclips(clips)
     final_clip.write_videofile("./Final.mp4", fps=24)
-    
 
-video_images_label = customtkinter.CTkLabel(master=generate_video, text="Select Images Path", font=("Roboto", 15, 'bold'))
+
+video_images_label = customtkinter.CTkLabel(
+    master=generate_video, text="Select Images Path", font=("Roboto", 15, 'bold'))
 video_images_label.pack(pady=10)
 
-video_image_entry = customtkinter.CTkEntry(master=generate_video, width=270, font=("Roboto", 15))
+video_image_entry = customtkinter.CTkEntry(
+    master=generate_video, width=270, font=("Roboto", 15))
 video_image_entry.configure(state=DISABLED)
 video_image_entry.pack(pady=10)
 
-video_image_location = customtkinter.CTkButton(master=generate_video, text='Set Location', font=("Roboto", 15), command=get_video_images_location)
+video_image_location = customtkinter.CTkButton(
+    master=generate_video, text='Set Location', font=("Roboto", 15), command=get_video_images_location)
 video_image_location.pack(pady=10)
 
-video_voice_label = customtkinter.CTkLabel(master=generate_video, text="Select Voices Path", font=("Roboto", 15, 'bold'))
+video_voice_label = customtkinter.CTkLabel(
+    master=generate_video, text="Select Voices Path", font=("Roboto", 15, 'bold'))
 video_voice_label.pack(pady=10)
 
-video_audios_entry = customtkinter.CTkEntry(master=generate_video, width=270, font=("Roboto", 15))
+video_audios_entry = customtkinter.CTkEntry(
+    master=generate_video, width=270, font=("Roboto", 15))
 video_audios_entry.configure(state=DISABLED)
 video_audios_entry.pack(pady=10)
 
-video_audio_location = customtkinter.CTkButton(master=generate_video, text='Set Location', font=("Roboto", 15), command=get_voices_location)
+video_audio_location = customtkinter.CTkButton(
+    master=generate_video, text='Set Location', font=("Roboto", 15), command=get_voices_location)
 video_audio_location.pack()
+
 
 def video_start():
     print(video_images_location)
     print(voices_location)
-    if not video_images_location or  not voices_location:
-        tkinter.messagebox.showerror(title='Error', message='Please set images and video location location')
+    if not video_images_location or not voices_location:
+        tkinter.messagebox.showerror(
+            title='Error', message='Please set images and video location location')
         return
-    
+
     thread = threading.Thread(target=create_video)
     thread.start()
 
-video_start_btn = customtkinter.CTkButton(master=generate_video, text='Start', font=("Roboto", 20, 'bold'), width=220, height=40, command=video_start)
+
+video_start_btn = customtkinter.CTkButton(master=generate_video, text='Start', font=(
+    "Roboto", 20, 'bold'), width=220, height=40, command=video_start)
 video_start_btn.pack(pady=25)
 
 
 video_progressbar = Progressbar(master=generate_video, length=300, value=0)
 video_progressbar.pack(pady=8, padx=8)
 
-video_progress = customtkinter.CTkLabel(master=generate_video, text='0%', font=("Roboto", 16, 'bold'))
+video_progress = customtkinter.CTkLabel(
+    master=generate_video, text='0%', font=("Roboto", 16, 'bold'))
 video_progress.pack(pady=5)
 
 # Others
 
-clear_label = customtkinter.CTkLabel(master=clear_text, text="Clear Text", font=("Roboto", 24, 'bold'))
+clear_label = customtkinter.CTkLabel(
+    master=clear_text, text="Clear Text", font=("Roboto", 24, 'bold'))
 clear_label.pack(pady=15, padx=10)
 
-clear_entry = customtkinter.CTkEntry(master=clear_text, width=270, font=("Roboto", 15))
+clear_entry = customtkinter.CTkEntry(
+    master=clear_text, width=270, font=("Roboto", 15))
 clear_entry.configure(state=DISABLED)
 clear_entry.pack(pady=10)
 
-clear_location = customtkinter.CTkButton(master=clear_text, text='Import Images', font=("Roboto", 15), command=import_images)
+clear_location = customtkinter.CTkButton(
+    master=clear_text, text='Import Images', font=("Roboto", 15), command=import_images)
 clear_location.pack(pady=10)
 
-language_label = customtkinter.CTkLabel(master=clear_text, text="Select Language", font=("Roboto", 15, 'bold'))
+language_label = customtkinter.CTkLabel(
+    master=clear_text, text="Select Language", font=("Roboto", 15, 'bold'))
 language_label.pack(pady=10)
 
-language_menu = customtkinter.CTkComboBox(master=clear_text, width=270, values=["English", "Japanese", "Korean", "Russian"])
+language_menu = customtkinter.CTkComboBox(master=clear_text, width=270, values=[
+                                          "English", "Japanese", "Korean", "Russian"])
 language_menu.pack(pady=10)
 
-clear_slider_label = customtkinter.CTkLabel(master=clear_text, text="Confidence: 0.15", font=("Roboto", 15, 'bold'))
+clear_slider_label = customtkinter.CTkLabel(
+    master=clear_text, text="Confidence: 0.15", font=("Roboto", 15, 'bold'))
 clear_slider_label.pack(pady=10)
 
-clear_slider = customtkinter.CTkSlider(master=clear_text, from_=0, to=1, number_of_steps=100, command=set_confidence)
+clear_slider = customtkinter.CTkSlider(
+    master=clear_text, from_=0, to=1, number_of_steps=100, command=set_confidence)
 clear_slider.pack()
 
-clear_start_btn = customtkinter.CTkButton(master=clear_text, text='Start', font=("Roboto", 20, 'bold'), width=220, height=40, command=clear_start)
+clear_start_btn = customtkinter.CTkButton(master=clear_text, text='Start', font=(
+    "Roboto", 20, 'bold'), width=220, height=40, command=clear_start)
 clear_start_btn.pack(pady=25)
 
 clear_progressbar = Progressbar(master=clear_text, length=300, value=0)
 clear_progressbar.pack(pady=8, padx=8)
 
-clear_progress = customtkinter.CTkLabel(master=clear_text, text='0%', font=("Roboto", 16, 'bold'))
+clear_progress = customtkinter.CTkLabel(
+    master=clear_text, text='0%', font=("Roboto", 16, 'bold'))
 clear_progress.pack(pady=5)
 
 root.mainloop()
